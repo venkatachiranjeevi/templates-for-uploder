a8f998e03d05cb861b8132424d2ddf3b743e6ef5
Venkat <kokku2chiranjeevi@gmail.com>
2014-12-17 11:15:08 +0530

    project status upto wednesday

---
+++ Template1Script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+  autoplay: 3000,
+    navigation : true,
+    singleItem : true,
+    transitionStyle : "fade"
+  });
+ 
+});
